c997f77b05fb573fbdee203baf93343372cc61d5
weifenluo <weifenluo@764a741f-c628-0410-a7dc-abe640748d39>
2007-03-09 02:22:43 +0000

    Bug fix: DockContent dock to itself's pane.

--- trunk/DockSample/Properties/AssemblyInfo.cs
+++ trunk/DockSample/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyFileVersion("2.0.0.0")]
