f0ed6537264f8dc73470e63111e0a94bba71bc39
weifenluo <weifenluo@764a741f-c628-0410-a7dc-abe640748d39>
2007-03-27 15:20:25 +0000

    XP Theme support.

--- WinFormsUI/Docking/InertButtonBase.cs
+++ WinFormsUI/Docking/InertButtonBase.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace WeifenLuo.WinFormsUI.Docking
 {
-    internal abstract class InertButtonBase : Label
+    internal abstract class InertButtonBase : Control
     {
-        protected InertButtonBase(ImageList imageList)
+        protected InertButtonBase()
         {
-            ImageList = imageList;
-            FlatStyle = FlatStyle.Flat;
-            Size = imageList.Images[0].Size;
-            BorderStyle = BorderStyle.None;
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
             BackColor = Color.Transparent;
+        }
+
+        public abstract Bitmap Image
+        {
+            get;
         }
 
         private bool m_isMouseOver = false;
@@ -27,8 +30,13 @@ namespace WeifenLuo.WinFormsUI.Docking
                     return;
 
                 m_isMouseOver = value;
-                OnRefreshChanges();
+                Invalidate();
             }
+        }
+
+        protected override Size DefaultSize
+        {
+            get { return Resources.DockPane_Close.Size; }
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
@@ -53,13 +61,48 @@ namespace WeifenLuo.WinFormsUI.Docking
                 IsMouseOver = false;
         }
 
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (IsMouseOver && Enabled)
+            {
+                using (Pen pen = new Pen(ForeColor))
+                {
+                    e.Graphics.DrawRectangle(pen, Rectangle.Inflate(ClientRectangle, -1, -1));
+                }
+            }
+
+            using (ImageAttributes imageAttributes = new ImageAttributes())
+            {
+                ColorMap[] colorMap = new ColorMap[2];
+                colorMap[0] = new ColorMap();
+                colorMap[0].OldColor = Color.FromArgb(0, 0, 0);
+                colorMap[0].NewColor = ForeColor;
+                colorMap[1] = new ColorMap();
+                colorMap[1].OldColor = Image.GetPixel(0, 0);
+                colorMap[1].NewColor = Color.Transparent;
+
+                imageAttributes.SetRemapTable(colorMap);
+
+                e.Graphics.DrawImage(
+                   Image,
+                   new Rectangle(0, 0, Image.Width, Image.Height),
+                   0, 0,
+                   Image.Width,
+                   Image.Height,
+                   GraphicsUnit.Pixel,
+                   imageAttributes);
+            }
+
+            base.OnPaint(e);
+        }
+
         public void RefreshChanges()
         {
             bool mouseOver = ClientRectangle.Contains(PointToClient(Control.MousePosition));
             if (mouseOver != IsMouseOver)
                 IsMouseOver = mouseOver;
-            else
-                OnRefreshChanges();
+
+            OnRefreshChanges();
         }
 
         protected virtual void OnRefreshChanges()
