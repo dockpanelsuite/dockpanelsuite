78291a0d6797d4df29a8619fe772eccc0f0c2445
Ryan Rastedt <ryan@rokendesign.com>
2012-07-08 20:57:24 -0500

    Cleanup for .NET 2.0 compilation errors
    Removed unused Linq namespace "using" statements that were breaking the
    build under .NET 2.0.  Targeting .NET 2.0 explicitly for the assembly.

--- WinFormsUI/Docking/DockPanel.Appearance.cs
+++ WinFormsUI/Docking/DockPanel.Appearance.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using WeifenLuo.WinFormsUI.Docking.Skins;
+﻿using WeifenLuo.WinFormsUI.Docking.Skins;
 using System.ComponentModel;
 
 namespace WeifenLuo.WinFormsUI.Docking
