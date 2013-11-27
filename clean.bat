f4d50c8d9f9bf30767906eacf17e0f3a72403e4b
Ryan Rastedt <ryan@rastedt.com>
2013-11-27 08:19:47 -0600

    Update build scripts to point to new solution

--- clean.bat
+++ clean.bat
@@ -1,4 +1,4 @@
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-call %MSBuildDir%\msbuild WinFormsUI.Docking.vs2010.sln /t:clean
-call %MSBuildDir%\msbuild WinFormsUI.Docking.vs2010.sln /t:clean /p:Configuration=Release
+call %MSBuildDir%\msbuild WinFormsUI.Docking.sln /t:clean
+call %MSBuildDir%\msbuild WinFormsUI.Docking.sln /t:clean /p:Configuration=Release
 @IF %ERRORLEVEL% NEQ 0 PAUSE
--- debug.bat
+++ debug.bat
@@ -1,3 +1,3 @@
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
-call %MSBuildDir%\msbuild WinFormsUI.Docking.vs2010.sln /t:build /p:Configuration="Debug"
+call %MSBuildDir%\msbuild WinFormsUI.Docking.sln /t:build /p:Configuration="Debug"
 @IF %ERRORLEVEL% NEQ 0 PAUSE
