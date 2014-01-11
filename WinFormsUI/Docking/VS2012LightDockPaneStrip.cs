a9c3db190bdb82e6b2b48e8b576d811476b26690
Lex Li <support@lextm.com>
2014-01-11 10:40:40 +0800

    Fixed document icon display issue.

--- WinFormsUI/Docking/VS2012LightDockPaneStrip.cs
+++ WinFormsUI/Docking/VS2012LightDockPaneStrip.cs
@@ -110,7 +110,7 @@ namespace WeifenLuo.WinFormsUI.Docking
         private const int _DocumentIconGapLeft = 8;
         private const int _DocumentIconGapRight = 0;
         private const int _DocumentIconHeight = 16;
-        private const int _DocumentIconWidth = 0;
+        private const int _DocumentIconWidth = 16;
         private const int _DocumentTextGapRight = 6;
 
         #endregion
