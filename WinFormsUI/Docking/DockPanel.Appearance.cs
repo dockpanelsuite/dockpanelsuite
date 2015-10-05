22e5f8fff76bae14e4c25d7b07a0b2f72f6ff5c1
Lex Li <support@lextm.com>
2015-10-05 12:40:50 +0800

    Set DesignerSerializationVisibility.Hidden on DockPanel.Skin property. Close #300.

--- WinFormsUI/Docking/DockPanel.Appearance.cs
+++ WinFormsUI/Docking/DockPanel.Appearance.cs
@@ -2,11 +2,14 @@
 
 namespace WeifenLuo.WinFormsUI.Docking
 {
+    using System.ComponentModel;
+
     public partial class DockPanel
     {
         private DockPanelSkin m_dockPanelSkin = VS2005Theme.CreateVisualStudio2005();
         [LocalizedCategory("Category_Docking")]
         [LocalizedDescription("DockPanel_DockPanelSkin")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         [Obsolete("Please use Theme instead.")]
         public DockPanelSkin Skin
         {
