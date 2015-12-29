395735d10976706db077775941f9a586c2b68bc6
Lex Li <support@lextm.com>
2015-12-29 19:56:01 +0800

    Updated NuGet definitions and prepared for beta 2 release.

--- nuget.bat
+++ nuget.bat
@@ -2,6 +2,7 @@ mkdir .nuget
 cd .nuget
 nuget pack DockPanelSuite.nuspec
 nuget pack ThemeVS2003.nuspec
+nuget pack ThemeVS2005Multithreading.nuspec
 nuget pack ThemeVS2012Light.nuspec
 nuget pack ThemeVS2013Blue.nuspec
 cd ..
