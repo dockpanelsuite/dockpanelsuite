7eed3b90f41f450b3dae8b3a88bec32a55ec61ad
Lex Li <support@lextm.com>
2016-08-20 17:18:00 +0800

    Added NuGet package restore.

--- debug.bat
+++ debug.bat
@@ -1,3 +1,5 @@
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+call .nuget\NuGet.exe update /self
+call .nuget\NuGet.exe restore
 call %MSBuildDir%\msbuild WinFormsUI.Docking.sln /t:build /p:Configuration="Debug"
 @IF %ERRORLEVEL% NEQ 0 PAUSE
--- release.net40.bat
+++ release.net40.bat
@@ -1,3 +1,5 @@
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
+call .nuget\NuGet.exe update /self
+call .nuget\NuGet.exe restore
 call %MSBuildDir%\msbuild WinFormsUI.Docking.sln /t:build /p:Configuration=Release /p:TargetFrameworkVersion=v4.0 /p:OutputPath=..\bin\net40\
 @IF %ERRORLEVEL% NEQ 0 PAUSE
