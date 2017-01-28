041ab866aa6e9ccc545d478dcb1ca0c44ad0fccc
Lex Li <support@lextm.com>
2017-01-28 20:37:44 +0800

    Bumped the version.

--- WinFormsUI/Properties/AssemblyInfo.cs
+++ WinFormsUI/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: AssemblyTitle("DockPanel Suite for .Net")]
-[assembly: AssemblyDescription(".Net Docking Library for Windows Forms")]
+[assembly: AssemblyDescription(".NET Docking Library for Windows Forms")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weifen Luo")]
 [assembly: AssemblyProduct("DockPanel Suite")]
-[assembly: AssemblyCopyright("Copyright © Weifen Luo and other contributors 2007-2016")]
+[assembly: AssemblyCopyright("Copyright © Weifen Luo and other contributors 2007-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -17,8 +17,8 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: Guid("9d690ef9-ce19-4c69-874c-e24d8eb36aff")]
 
-[assembly: AssemblyVersion("2.11.0.0")]
-[assembly: AssemblyFileVersion("2.11.0.0")]
+[assembly: AssemblyVersion("2.13.0.0")]
+[assembly: AssemblyFileVersion("2.13.0.0")]
 
 [module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "WeifenLuo.WinFormsUI.Docking", MessageId = "Weifen")]
 [module: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", Scope = "namespace", Target = "WeifenLuo.WinFormsUI.Docking", MessageId = "Luo")]
