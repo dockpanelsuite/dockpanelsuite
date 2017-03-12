531535e7e3fd412ece2d9651e4b269798c8c6835
Ramin Dalkouhi <romout@gmail.com>
2017-03-12 14:17:19 +0100

    ThreadStatic attribute and set hook to null when being disposed

--- WinFormsUI/Docking/DockPanel.FocusManager.cs
+++ WinFormsUI/Docking/DockPanel.FocusManager.cs
@@ -120,6 +120,7 @@ namespace WeifenLuo.WinFormsUI.Docking
             // Use a static instance of the windows hook to prevent stack overflows in the windows kernel.
             [ThreadStatic]
             private static LocalWindowsHook sm_localWindowsHook;
+            [ThreadStatic]
             private static int _referenceCount = 0;
 
             private readonly LocalWindowsHook.HookEventHandler m_hookEventHandler;
@@ -161,8 +162,11 @@ namespace WeifenLuo.WinFormsUI.Docking
                     --_referenceCount;
 
                     if (_referenceCount == 0)
+                    {
                         sm_localWindowsHook.Dispose();
-                    
+                        sm_localWindowsHook = null;
+                    }
+					
                     m_disposed = true;
                 }
 
