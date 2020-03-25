214a2f67f33a1ee491c98a298df13e1591b96161
Lex Li <support@lextm.com>
2020-03-24 21:12:43 -0400

    Updated the sample project to avoid obsolete MainMenu type.

--- DockSample/DummyPropertyWindow.Designer.cs
+++ DockSample/DummyPropertyWindow.Designer.cs
@@ -31,8 +31,8 @@ namespace DockSample
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DummyPropertyWindow));
             this.propertyGrid = new System.Windows.Forms.PropertyGrid();
             this.comboBox = new System.Windows.Forms.ComboBox();
-            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
-            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.mainMenu1 = new System.Windows.Forms.MenuStrip();
+            this.menuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.SuspendLayout();
             // 
             // propertyGrid
@@ -57,12 +57,11 @@ namespace DockSample
             // 
             // mainMenu1
             // 
-            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.mainMenu1.Items.AddRange(new System.Windows.Forms.ToolStripMenuItem[] {
             this.menuItem1});
             // 
             // menuItem1
             // 
-            this.menuItem1.Index = 0;
             this.menuItem1.Text = "File";
             // 
             // DummyPropertyWindow
@@ -76,7 +75,7 @@ namespace DockSample
                         | WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom)));
             this.HideOnClose = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Menu = this.mainMenu1;
+            this.MainMenuStrip = this.mainMenu1;
             this.Name = "DummyPropertyWindow";
             this.Padding = new System.Windows.Forms.Padding(0, 3, 0, 3);
             this.ShowHint = WeifenLuo.WinFormsUI.Docking.DockState.DockRight;
@@ -89,7 +88,7 @@ namespace DockSample
 
         private System.Windows.Forms.PropertyGrid propertyGrid;
         private System.Windows.Forms.ComboBox comboBox;
-        private System.Windows.Forms.MainMenu mainMenu1;
-        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuStrip mainMenu1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem1;
     }
 }
