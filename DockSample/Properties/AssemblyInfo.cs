c385897aa172a39fcdf786ffea89a90b6252a73c
Scott Stephens <stephens.js@gmail.com>
2021-02-17 18:13:32 -0600

    Switch DockSample to SDK style csproj

--- DockSample/Properties/AssemblyInfo.cs
+++ DockSample/Properties/AssemblyInfo.cs
@@ -29,5 +29,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.*")]
-//[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
