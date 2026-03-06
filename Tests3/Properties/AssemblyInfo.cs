4eafae9b69e1f86faea70685a9dd334cde7d169a
Ed-bb <70423944+Ed-bb@users.noreply.github.com>
2026-03-06 15:57:23 -0600

    Fixing Tests3
    Add ComVisible attribute and clarify with comments
    
    Added explanatory comments for ComVisible and set it to false in AssemblyInfo.cs. Also made a minor formatting adjustment after AssemblyFileVersion.

--- Tests3/Properties/AssemblyInfo.cs
+++ Tests3/Properties/AssemblyInfo.cs
@@ -14,7 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2ef6dd60-e690-4e65-ad06-b3041c0acdd2")]
